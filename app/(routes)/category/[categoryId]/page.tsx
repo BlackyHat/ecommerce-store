f0cf1cda652416ae9457a59f72ac57d8f0ed19ce
Oleksandr P <goodwin009@gmail.com>
2023-08-21 16:34:38 +0300

    update category page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from '@/components/ui/container';
 import Filter from './components/filter';
 import NoResults from '@/components/ui/no-results';
 import ProductCard from '@/components/ui/product-card';
+import MobileFilters from './components/mobile-filters';
 
 interface CategoryPageProps {
   params: {
@@ -36,9 +37,10 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter name="Sizes" data={sizes} valueKey="sizeId" />
-              <Filter name="Colors" data={colors} valueKey="colorId" />
+              <Filter name="Colors" data={colors} valueKey="colorId" />M
             </div>
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
--- components/billboard.tsx
+++ components/billboard.tsx
@@ -12,9 +12,9 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
         className="relative rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-bottom"
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xs">
+          <p className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xs ">
             {data.label}
-          </div>
+          </p>
         </div>
       </div>
     </div>
