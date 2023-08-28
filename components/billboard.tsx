56527cc3c2d0997489d87a3d4ebfe73fd6f10bf7
Oleksandr P <goodwin009@gmail.com>
2023-08-28 16:18:31 +0300

    fix info ui

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -12,7 +12,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
         className="relative rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-bottom"
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <p className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xs ">
+          <p className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-md text-white backdrop-blur-sm">
             {data.label}
           </p>
         </div>
