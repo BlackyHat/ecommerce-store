555cf174bd042f6d220b0cc5a5b9bb36f648ffe8
Oleksandr P <goodwin009@gmail.com>
2024-03-19 18:25:21 +0200

    refactor Info component

--- components/base/Info/Info.tsx
+++ components/base/Info/Info.tsx
@@ -2,56 +2,48 @@ import { ShoppingCart } from 'lucide-react'
 
 import { Button, Currency } from '@/components/ui'
 
+import content from '@/data/common.json'
+
 import { InfoProps } from './types'
 
-export const Info: React.FC<InfoProps> = ({ data }) => (
-  <div>
-    <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+export const Info: React.FC<InfoProps> = ({ data }) => {
+  const { button, props } = content.infoSection
 
-    <div className="mt-3 flex items-end justify-between">
-      <div className="text-2xl text-gray-900">
-        <Currency value={data?.price} />
+  const labels: Record<string, string | undefined> = {
+    Category: data.category.name,
+    'Body Type': data.bodyType.label,
+    Make: data.make.label,
+    Model: data.model.label,
+    Color: data.color.name,
+  }
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+
+      <div className="mt-3 flex items-end justify-between">
+        <div className="text-2xl text-gray-900">
+          <Currency value={data?.price} />
+        </div>
+      </div>
+
+      <hr className="my-4" />
+
+      <ul className="flex flex-col gap-y-6">
+        {props.map(prop => (
+          <li key={prop} className="flex items-center gap-x-4">
+            <h3 className="font-semibold text-black capitalize">{prop}:</h3>
+
+            <p>{labels[prop]}</p>
+          </li>
+        ))}
+      </ul>
+
+      <div className="mt-10 flex items-center gap-x-3">
+        <Button className="flex items-center gap-x-2">
+          {button.label} <ShoppingCart />
+        </Button>
       </div>
     </div>
-
-    <hr className="my-4" />
-
-    <ul className="flex flex-col gap-y-6">
-      <li className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Category:</h3>
-
-        <p>{data.category.name}</p>
-      </li>
-
-      <li className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Body Type:</h3>
-
-        <p>{data.bodyType.label}</p>
-      </li>
-
-      <li className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Make:</h3>
-
-        <p>{data.make.label}</p>
-      </li>
-
-      <li className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Model:</h3>
-
-        <p>{data.model.label}</p>
-      </li>
-
-      <li className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Color:</h3>
-
-        <p>{data.color.name}</p>
-      </li>
-    </ul>
-
-    <div className="mt-10 flex items-center gap-x-3">
-      <Button className="flex items-center gap-x-2">
-        Add To Favotite <ShoppingCart />
-      </Button>
-    </div>
-  </div>
-)
+  )
+}
--- sections/CarOverview/CarOverview.tsx
+++ sections/CarOverview/CarOverview.tsx
@@ -9,10 +9,10 @@ export const CarOverview: React.FC<CarOverviewProps> = ({
   <section className="section">
     <div className="container">
       <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-        {/* <Gallery images={product.images} /> */}
+        <Gallery images={product.images} />
 
         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-          {/* <Info data={product} /> */}
+          <Info data={product} />
         </div>
       </div>
       <hr className="my-10" />
